ea3b0ef38a1e8dbc977c98a69302272859b1aa40
Ghassan <rassen92@gmail.com>
2022-09-12 10:59:34 +0200

    save des docs

--- models/countries.js
+++ models/countries.js
@@ -1,13 +1,17 @@
 // Insert your code here
-const =mongoose = require('mongoose');
+const mongoose = require('mongoose');
+const PolulationbyYear = mongoose.Schema({
+    population:Number,
+    year:Date,
+}) 
 
 const countries = mongoose.Schema({
     name:String,
     flagImg:String,
     currency:String,
-    population:Number,
+    population:[PolulationbyYear],
 })
 
-
+const Country = mongoose.model('countries', countrySchema)
 
 module.exports = Country
